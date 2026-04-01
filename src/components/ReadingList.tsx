9454eec78f4853e3bb062471a797a5b4534d710c
serious-pavel <pvmakhnev@gmail.com>
2026-04-01 11:32:33 +0200

    adjust className

--- src/components/ReadingList.tsx
+++ src/components/ReadingList.tsx
@@ -31,7 +31,7 @@ const ReadingList = ({readings, selectedReadingId, setBPList, setSelectedReading
             console.log("Selected")
           }}
           key={reading.id}
-          className={reading.id === selectedReadingId ? 'bpListItem selected' : 'bpListItem'}
+          className={`bpListItem ${reading.id === selectedReadingId ? 'selected' : ''}`}
         >
           {getShortDate(reading.time)} {getShortTime(reading.time)}: {reading.sys}/{reading.dia}
           <button onClick={(e) => {
